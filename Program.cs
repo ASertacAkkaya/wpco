dbdb3d23fc1fea5d931291eb0c613addc86c3783
A. Sertaç Akkaya <anilsertac.a@gmail.com>
2024-08-14 19:44:34 +0300

    Release version 1.0.0

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp;
+
+namespace wpco
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            string[] extensions = { "*.webp" };
+            string[] matchedFiles = extensions.SelectMany(ext => Directory.GetFiles(currentDirectory, ext)).ToArray();
+
+            if (matchedFiles.Length == 0)
+            {
+                Console.WriteLine("There are no .webp files in the current directory.");
+                return;
+            }
+
+            foreach (string file in matchedFiles)
+            {
+                string outputPathWithoutExt = Path.Combine(currentDirectory, Path.GetFileNameWithoutExtension(file));
+
+                try
+                {
+                    using (Image<Rgba32> image = Image.Load<Rgba32>(file))
+                    {
+                        Console.WriteLine($"Processing {file}...");
+                        int frameCount = image.Frames.Count;
+
+                        if (frameCount > 1)
+                        {
+                            Console.WriteLine("Detected an animated WebP, converting to GIF...");
+                            image.SaveAsGif(outputPathWithoutExt + ".gif");
+                            Console.WriteLine($"Saved to {outputPathWithoutExt}.gif");
+                        }
+                        else
+                        {
+                            image.SaveAsPng(outputPathWithoutExt + ".png");
+                            Console.WriteLine($"Saved to {outputPathWithoutExt}.png");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An error occured: {ex.Message}");
+                }
+            }
+            Console.WriteLine("Process completed.");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# wpco - WebP Converter
+
+## Features
+- Converts WebP images into PNG.
+- Autodetection for animated WebP files to be converted to GIF files.
+
+## Features to come
+- Drag-and-drop support.
+- Command-line configurations.
